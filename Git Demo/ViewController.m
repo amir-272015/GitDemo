02489d21a3da27027980255c7c6c1312831ef524
sierra <AMIR@amir.local>
2017-06-21 19:47:22 +0530

    After revert.

--- Git Demo/ViewController.m
+++ Git Demo/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"Hello World");
-    NSLog(@"Update Branch");
+    NSLog(@"Update Branch after revert");
 }
 
 
